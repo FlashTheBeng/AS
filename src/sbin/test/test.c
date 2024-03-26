9c3ce8225900f12fb86f2dad4fcce7c8f66f7df5
Clement Dehosse <clem.dhs59@gmail.com>
2024-03-26 22:06:38 +0100

    remove print

--- src/sbin/test/test.c
+++ src/sbin/test/test.c
@@ -51,7 +51,7 @@ static unsigned flags = VERBOSE | FULL;
  */
 static int swap_test(void)
 {
-	#define N 100
+	#define N 500
 	int *a, *b, *c;
 	clock_t t0, t1;
 	struct tms timing;
@@ -84,7 +84,6 @@ static int swap_test(void)
 
 				for (int k = 0; k < N; k++){
 					c[i*N + j] += a[i*N + k]*b[k*N + j];
-					printf("test4");
 				}
 					
 			}
